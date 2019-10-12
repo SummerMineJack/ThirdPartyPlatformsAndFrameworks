a8ce29bbbe9e352fa253cb1cdae88c39014988ab
HuangJian <45996603+SummerMineJack@users.noreply.github.com>
2019-10-12 13:29:59 +0800

    1，完善字体练习

--- app/src/main/java/com/ungpay/thirdpartyplatformsandframeworks/MyApplication.java
+++ app/src/main/java/com/ungpay/thirdpartyplatformsandframeworks/MyApplication.java
@@ -5,7 +5,6 @@ import android.util.Log;
 
 import com.blankj.utilcode.util.CrashUtils;
 import com.lzy.okgo.OkGo;
-import com.ungpay.thirdpartyplatformsandframeworks.ui.fonts.CustomViewWithTypefaceSupport;
 import com.ungpay.thirdpartyplatformsandframeworks.ui.fonts.TextField;
 
 import cn.finalteam.okhttpfinal.OkHttpFinal;
@@ -21,11 +20,9 @@ public class MyApplication extends Application {
         OkHttpFinal.getInstance().init(builder.build());
         OkGo.getInstance().init(this);
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
-                        .setDefaultFontPath("fonts/Lato-Regular.ttf")
-                        .setFontAttrId(R.attr.fontPath)
-                        .addCustomViewWithSetTypeface(CustomViewWithTypefaceSupport.class)
-                        .addCustomStyle(TextField.class, R.attr.textFieldStyle)
-                        .build());
+                .setDefaultFontPath("fonts/Lato-Regular.ttf")
+                .addCustomStyle(TextField.class, R.attr.textFieldStyle)
+                .build());
 
         CrashUtils.init(new CrashUtils.OnCrashListener() {
             @Override
--- app/src/main/java/com/ungpay/thirdpartyplatformsandframeworks/ui/fonts/FontsActivity.java
+++ app/src/main/java/com/ungpay/thirdpartyplatformsandframeworks/ui/fonts/FontsActivity.java
@@ -2,11 +2,18 @@ package com.ungpay.thirdpartyplatformsandframeworks.ui.fonts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.ungpay.thirdpartyplatformsandframeworks.R;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
 public class FontsActivity extends AppCompatActivity {
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
