4e8a550794d8b71b40dda540ad0422f46713c27f
HuangJian <45996603+SummerMineJack@users.noreply.github.com>
2019-12-06 17:31:48 +0800

    1，修改部分代码

---
+++ app/src/main/java/com/ungpay/thirdpartyplatformsandframeworks/ui/SimpleExoPlayerTimeBar.java
@@ -0,0 +1,16 @@
+package com.ungpay.thirdpartyplatformsandframeworks.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ungpay.thirdpartyplatformsandframeworks.R;
+
+public class SimpleExoPlayerTimeBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_exo_player_time_bar);
+    }
+}
--- app/src/main/java/com/ungpay/thirdpartyplatformsandframeworks/ui/UiActivity.java
+++ app/src/main/java/com/ungpay/thirdpartyplatformsandframeworks/ui/UiActivity.java
@@ -3,52 +3,68 @@ package com.ungpay.thirdpartyplatformsandframeworks.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ungpay.thirdpartyplatformsandframeworks.R;
+import com.ungpay.thirdpartyplatformsandframeworks.adapter.DemoListAdapter;
 import com.ungpay.thirdpartyplatformsandframeworks.ui.PhoneCard.EditTextWathcerActivity;
 import com.ungpay.thirdpartyplatformsandframeworks.ui.ViewLayoutTransation.ViewActivity;
 import com.ungpay.thirdpartyplatformsandframeworks.ui.alipayui.AlipayHomeActivity;
 import com.ungpay.thirdpartyplatformsandframeworks.ui.customKeyBoard.CustomKeyboard;
 import com.ungpay.thirdpartyplatformsandframeworks.ui.fonts.FontsActivity;
 
-public class UiActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class UiActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView demoList;
+    private ArrayList<String> datas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ui);
-        findViewById(R.id.alipay_home).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(UiActivity.this, AlipayHomeActivity.class));
-            }
-        });
-        findViewById(R.id.fonts_home).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(UiActivity.this, FontsActivity.class));
-            }
-        });
-        findViewById(R.id.editText).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(UiActivity.this, EditTextWathcerActivity.class));
-            }
-        });
-        findViewById(R.id.ViewShowHidden).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(UiActivity.this, ViewActivity.class));
-            }
-        });
-        findViewById(R.id.keyboard).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(UiActivity.this, CustomKeyboard.class));
-            }
-        });
+        demoList = findViewById(R.id.list_demo);
+        demoList.setOnItemClickListener(this);
+        initData();
+        demoList.setAdapter(new DemoListAdapter(datas, UiActivity.this));
 
+    }
+
+    private void initData() {
+        datas = new ArrayList<>();
+        datas.add("仿支付宝首页滑动缩放");
+        datas.add("字体");
+        datas.add("editText");
+        datas.add("ViewShowHidden");
+        datas.add("keyboard");
+        datas.add("SimpleExoPlayerTimeBar");
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                startActivity(new Intent(UiActivity.this, AlipayHomeActivity.class));
+                break;
+            case 1:
+                startActivity(new Intent(UiActivity.this, FontsActivity.class));
+                break;
+            case 2:
+                startActivity(new Intent(UiActivity.this, EditTextWathcerActivity.class));
+                break;
+            case 3:
+                startActivity(new Intent(UiActivity.this, ViewActivity.class));
+                break;
+            case 4:
+                startActivity(new Intent(UiActivity.this, CustomKeyboard.class));
+                break;
+            case 5:
+                startActivity(new Intent(UiActivity.this, SimpleExoPlayerTimeBar.class));
+                break;
+        }
     }
 }
